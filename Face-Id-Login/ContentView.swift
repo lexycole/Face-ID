b3e728face37b7b4fc42340b57da28415c862a3e
Olawale Akinro <akinbolawa90@gmail.com>
2022-08-15 00:42:02 -0700

    Update Description

--- Face-Id-Login/ContentView.swift
+++ Face-Id-Login/ContentView.swift
@@ -9,7 +9,7 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Text("This app utilise face ID Authentication")
+        Text("This app utilize face ID Authentication")
             .padding()
     }
 }
